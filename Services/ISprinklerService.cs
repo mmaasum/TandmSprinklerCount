086488fe65de84ec65a7bca95392e1ba6292b8e9
Masum <61491491+mmaasum@users.noreply.github.com>
2026-02-10 02:58:02 +0600

    Update ISprinklerService.cs

--- Services/ISprinklerService.cs
+++ Services/ISprinklerService.cs
@@ -2,7 +2,7 @@ using static TandmSprinklerCount.Models.FireDesignModels;
 
 namespace TandmSprinklerCount.Services
 {
-    public record SprinklerLayout(Point3D coordinates);
+    public record SprinklerLayout(Point3D ConnectionPoint);
     public interface ISprinklerService
     {
         Task<IEnumerable<SprinklerLayout>> GenerateLayoutAsync();
